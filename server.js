677d1e7c851805746f334395483633e1d61635d9
sharanabasappadhammur <108877043+sharanabasappadhammur@users.noreply.github.com>
2025-03-04 12:17:58 +0530

    sd

--- server.js
+++ server.js
@@ -52,8 +52,6 @@ app.get("*", async (req, res) => {
   let htmlContent = fs.readFileSync(filePath, "utf8");
   const userAgent = req.headers["user-agent"].toLowerCase();
 
-  console.log(req.headers.host);
-
   if (
     (/facebook|fbav|fban|twitter|instagram|linkedin|whatsapp|snapchat|googlebot|bingbot|pinterest|reddit|tiktok/i.test(
       userAgent
