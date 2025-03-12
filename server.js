57f00a81c838aa8ba2d91ff443684c1b14399951
sharana <108877043+sharanabasappadhammur@users.noreply.github.com>
2025-03-12 13:15:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -142,6 +142,6 @@ app.get("*", async (req, res) => {
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(8000, () => {
+  console.log(`Server is running on http://localhost:${8000}`);
 });
